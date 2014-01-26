1f52564d28aef5819342105d3027873c24b66eb7
Luke Page <luke.a.page@gmail.com>
2014-01-26 08:30:26 +0000

    auto generate side nav bar

---
+++ templates/plugins/toc.js
@@ -0,0 +1,57 @@
+/**
+ * Heavily Adapted From https://github.com/assemble/assemble-contrib-toc
+ */
+
+var options = {
+    stage: 'render:post:page'
+};
+
+var cheerio = require('cheerio');
+
+module.exports = function(params, callback) {
+    'use strict';
+
+    // load current page content
+    var $ = cheerio.load(params.content);
+
+    // get all the anchor tags from inside the headers
+    var anchors = $('h1[id],h2[id],h3[id]'),
+        toc = [];
+
+    function findLocation(toc, depth) {
+        if (depth === 1) {
+            return toc;
+        }
+
+        var loc = toc[toc.length - 1];
+        if (!loc) {
+            loc = { children: [] };
+            toc.push(loc);
+        } else if (!loc.children) {
+            loc.children = [];
+        }
+        return findLocation(loc.children, depth - 1 );
+    }
+
+    anchors.map(function(i, e) {
+        var text  = $(e).text().trim();
+        var link  = e.attribs.id;
+        var depth = Math.min(2, parseInt(e.name.replace(/h/gi, ''), 10));
+        var location = findLocation(toc, depth);
+        location.push( { text: text, link: link });
+    });
+
+    function toHtml(toc, first) {
+        return "<ul class=\"nav" + (first ? " sidenav" : "") + "\">" + toc.map(function(loc) {
+            return '<li><a href="#' + loc.link + '">' + loc.text + '</a>' +
+                (loc.children ? toHtml(loc.children) : "") + '</li>';
+        }).join("") + "</ul>";
+    }
+
+    $("#toc").append(toHtml(toc, true));
+
+    params.content = $.html();
+    callback();
+};
+
+module.exports.options = options;
