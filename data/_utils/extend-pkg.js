c43b88a2d22f9bbcadeddabba33bb2720b57436c
Chris Mitchell <chris@cloudboltsoftware.com>
2014-09-23 14:46:41 -0700

    Fix broken links pointing to GitHub
    It seems like 2ec20ce228b removed a trailing slash from the `less.master`
    template variable. This broke many links, like ones to the changelog:
    
    ```md
    Released [v1.7.0]({{ less.master }}CHANGELOG.md)
    ```
    
    which rendered like:
    
    ```html
    <p>Released <a href="https://github.com/less/less.js/blob/masterCHANGELOG.md">v1.7.0</a></p>
    ```
    
    This commit re-adds the trailing slash to fix those links, and undoes a couple
    of spot changes that others made to fix individual links (changes that added a
    slash in the template instead of to the template variable).

--- data/_utils/extend-pkg.js
+++ data/_utils/extend-pkg.js
@@ -1,7 +1,7 @@
 module.exports = {
   repo: "https://github.com/less/less.js.git",
   issues: "https://github.com/less/less.js/issues",
-  master: "https://github.com/less/less.js/blob/master",
-  rawmaster: "https://raw.github.com/less/less.js/master/dist",
+  master: "https://github.com/less/less.js/blob/master/",
+  rawmaster: "https://raw.github.com/less/less.js/master/dist/",
   sourcearchive: "https://github.com/less/less.js/archive/v"
 };
